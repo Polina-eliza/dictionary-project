8034f6acb10ac1068007c3dd1c89b27fb1af8b10
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-03-10 12:53:18 +0200

    updated the app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from "./logo.png"
-import './App.css';
-import Dictionary from './Dictionary'
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-    <header className="App-header">
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
-    </header>
-    <main>
-      <Dictionary />
-    </main>
-    <footer className="text-center">Coded by Polina</footer>
-  </div>
-  </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Polina</small>
+        </footer>
+      </div>
+    </div>
   );
 }
-
-export default App;
